a58128b982fd414ee6cf236c16d18ba6283ce4f8
Rj-wtwuj <119797182+Rj-wtwuj@users.noreply.github.com>
2023-01-19 00:39:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 from os import getenv
 
-API_ID = int(getenv("API_ID", "")) #optional
-API_HASH = getenv("API_HASH", "") #optional
+API_ID = int(getenv("API_ID", "13146123")) #optional
+API_HASH = getenv("API_HASH", "53e10a257baaddea9c413a0c4249e731") #optional
 
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-MONGO_URL = getenv("MONGO_URL", "")
-OWNER_ID = int(getenv("OWNER_ID", ""))
-LOG_GROUP = getenv("LOG_GROUP", "")
-PM_LOGGER = getenv("PM_LOGGER", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "5663138972:AAHF9IbIfmzZUTxQuWtinZ3KaymRM1dYo0w")
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://Oyehoye143:Oyehoye143@cluster0.zxadbr9.mongodb.net/?retryWrites=true&w=majority")
+OWNER_ID = int(getenv("OWNER_ID", "5378060239"))
+LOG_GROUP = getenv("LOG_GROUP", "-1001876662705")
+PM_LOGGER = getenv("PM_LOGGER", "-1001876662705")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5368154755").split()))
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
 REPO_URL = getenv("REPO_URL", "https://github.com/Romeo-RJ/ROMEO-USERBOT")
@@ -15,7 +15,7 @@ BRANCH = getenv("BRANCH", "master") #don't change
 ALIVE_PIC = getenv("ALIVE_PIC", "https://telegra.ph/file/a62b9c7d9848afde0569e.jpg")
 ALIVE_TEXT = getenv("ALIVE_TEXT", "ʀᴏᴍᴇᴏʙᴏᴛ")
  
-STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION1 = getenv("STRING_SESSION1", "BQBaXXpPEg0xwzWfN-GpzXGz51n1hylX7NyAB3B2L3DG4VwiVqHCJ0DmtX2-apgceFIGHAJeMQbL4kcE2I-jGWJo_yhmCp-egwnuSORisQf7vcrsbrW_pjUI1LPf5fVxC4x5YZsHsgGwUrCI0qa7NVVHKfvXO2r3k2HencsVySZp3U9y8R7rKlydc87dcb7I1FrnolHaS-dJnYYNqzMbFQorV6OGve5J6VUSLE9u32HpWFzE9o2nooykVqpmN4-jePr2LCs6K-5KL11nnC5Kd28hTaYZlKiS13qULwEOB74wwpkLGpHEOARArZRRXOJJExbukaUnU_xojTEM29NJISQKAAAAAUCOr88A")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
 STRING_SESSION4 = getenv("STRING_SESSION4", "")
